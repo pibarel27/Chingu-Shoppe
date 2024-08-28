f89aa241793ad4c14b44c1ba08c606f084e352af
Pibarel Maisnam <112171422+pibarel27@users.noreply.github.com>
2024-08-28 18:48:37 +0530

    Update ItemCard.js

--- src/pages/Cart/ItemCard.js
+++ src/pages/Cart/ItemCard.js
@@ -5,7 +5,7 @@ import {
   deleteItem,
   drecreaseQuantity,
   increaseQuantity,
-} from "../../redux/orebiSlice";
+} from "../../redux/chinguSlice";
 
 const ItemCard = ({ item }) => {
   const dispatch = useDispatch();
