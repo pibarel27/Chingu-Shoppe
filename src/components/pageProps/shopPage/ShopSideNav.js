749f269925c06758c185ffe628e74118277fc479
Pibarel Maisnam <112171422+pibarel27@users.noreply.github.com>
2024-08-28 21:08:45 +0530

    Update ShopSideNav.js

--- src/components/pageProps/shopPage/ShopSideNav.js
+++ src/components/pageProps/shopPage/ShopSideNav.js
@@ -4,7 +4,6 @@ import Category from "./shopBy/Category";
 import Color from "./shopBy/Color";
 import Price from "./shopBy/Price";
 
-
 const ShopSideNav = () => {
   return (
     <div className="w-full flex flex-col gap-6">
