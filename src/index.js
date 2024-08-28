fc1e141329e145c6d17f117256b3b4f45bcf03b9
Pibarel Maisnam <112171422+pibarel27@users.noreply.github.com>
2024-08-28 19:46:51 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import { store, persistor } from "./redux/store";
 import "./index.css";
 import App from "./App";
 
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
